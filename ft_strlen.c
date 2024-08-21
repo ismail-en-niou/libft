b70f1833bae629de94763c0133b2d94dc3d8afa3
ismail-en-niou <ismail.enniou@etu.uae.ac.ma>
2024-08-21 14:49:37 +0100

    ft_strlen.c

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,26 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/21 14:13:36 by marvin            #+#    #+#             */
-/*   Updated: 2024/08/21 14:13:36 by marvin           ###   ########.fr       */
+/*   Created: 2024/08/21 14:45:30 by marvin            #+#    #+#             */
+/*   Updated: 2024/08/21 14:45:30 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_memcmp(const void *s1, const void *s2, size_t n)
+size_t	strlen(const char *s)
 {
-	size_t	x;
+	size_t	i;
 
-	x = -1;
-	while (++x < n)
-	{
-		if (((t_byte *)s1)[x] != ((t_byte *)s2)[x])
-			return (((t_byte *)s1)[x] - ((t_byte *)s2)[x]);
-	}
-	return (0);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
 }
