41d68674fcb77b34fca207688a13ac042d7ef4ed
ismail-en-niou <ismail.enniou@etu.uae.ac.ma>
2024-08-21 12:31:14 +0100

    new commit from zero to hero

--- ft_memset.c
+++ ft_memset.c
@@ -5,22 +5,16 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/20 12:49:59 by marvin            #+#    #+#             */
-/*   Updated: 2024/07/20 12:49:59 by marvin           ###   ########.fr       */
+/*   Created: 2024/08/21 12:17:01 by marvin            #+#    #+#             */
+/*   Updated: 2024/08/21 12:17:01 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
 #include "libft.h"
 
-void	*ft_memset(void *str, char c, size_t n)
+void	*ft_memset(void *s, int c, size_t n)
 {
-	unsigned char	*p;
-
-	p = (unsigned char *)str ;
-	while (n-- > 0)
-	{
-		*p++ = (unsigned char )c;
-	}
-	return (str);
+	while (n--)
+		(t_byte *)s[n] = (t_byte)c;
+	return (s);
 }
--- libft.h
+++ libft.h
@@ -5,36 +5,100 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/20 12:56:12 by marvin            #+#    #+#             */
-/*   Updated: 2024/07/20 12:56:12 by marvin           ###   ########.fr       */
+/*   Created: 2024/08/21 12:18:00 by marvin            #+#    #+#             */
+/*   Updated: 2024/08/21 12:18:00 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 # include <unistd.h>
-# include <stdlib.h>
 
-void	*ft_memset(void *str, char c, size_t n);
-void	*ft_calloc(size_t count, size_t size);
-void	ft_bzero(void *s, size_t n);
-int		ft_atoi(const char *str);
-int		ft_isalpha(int i);
-int		ft_isascii(int i);
-int		ft_isdigit(int i);
-int		ft_isprint(int i);
-char	*ft_itoa(int n);
-void	*ft_memchr(const void *s, int c, size_t n);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
-void	*ft_memmove(void *s1, const void *s2, size_t n);
-void	ft_putchar_fd(char c, int fd);
-int		ft_isalnum(int i);
-int		ft_toupper(int i);
-int		ft_tolower(int i);
-int		ft_strlen(char *str);
-size_t	ft_strlcpy(char *dest, const char *src, size_t size);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strdub(const char *s);
-char	*ft_strchr(const char *s, int i);
-char	**ft_split(char const *str, char c);
+typedef unsigned char	t_byte;
+/*
+** what it is
+*/
+
+int				ft_isupper(int c);
+int				ft_isspace(int c);
+int				ft_islower(int c);
+int				ft_isalnum(int c);
+int				ft_isalpha(int c);
+int				ft_isascii(int c);
+int				ft_isdigit(int c);
+int				ft_isprint(int c);
+int				ft_tolower(int c);
+int				ft_toupper(int c);
+
+/*
+** Memory
+*/
+
+void			ft_bzero(void *s, size_t n);
+void			*ft_memalloc(size_t size);
+void			*ft_memccpy(void *dst, const void *src, int c, size_t n);
+void			*ft_memchr(const void *s, int c, size_t n);
+int				ft_memcmp(const void *s1, const void *s2, size_t n);
+void			*ft_memcpy(void *dest, const void *src, size_t num);
+void			ft_memdel(void **ap);
+void			*ft_memmove(void *dest, const void *src, size_t n);
+void			*ft_memset(void *s, int c, size_t n);
+
+/*
+** Putin
+*/
+
+void			ft_putchar(char c);
+void			ft_putchar_fd(char c, int fd);
+void			ft_putendl(char const *s);
+void			ft_putendl_fd(char const *s, int fd);
+void			ft_putnbr(int nb);
+void			ft_putnbr_fd(int nb, int fd);
+void			ft_putstr(char const *s);
+void			ft_putstr_fd(char const *s, int fd);
+void			ft_print_memory(const void *addr, size_t size);
+
+/*
+** Str
+*/
+
+char			*ft_strcat(char *dest, const char *src);
+char			*ft_strchr(const char *s, int c);
+void			ft_strclr(char *s);
+int				ft_strcmp(const char *s1, const char *s2);
+char			*ft_strcpy(char *dest, const char *src);
+void			ft_strdel(char **as);
+char			*ft_strdup(const char *s);
+int				ft_strequ(char const *s1, char const *s2);
+void			ft_striter(char *s, void (*f)(char *));
+void			ft_striteri(char *s, void (*f)(unsigned int, char *));
+char			*ft_strjoin(char const *s1, char const *s2);
+size_t			ft_strlcat(char *dst, const char *src, size_t size);
+size_t			ft_strlen(const char *s);
+char			*ft_strmap(char const *s, char (*f)(char));
+char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char			*ft_strncat(char *dest, const char *src, size_t n);
+int				ft_strncmp(const char *s1, const char *s2, size_t size);
+char			*ft_strncpy(char *dest, const char *src, size_t n);
+int				ft_strnequ(char const *s1, char const *s2, size_t n);
+char			*ft_strnew(size_t size);
+char			*ft_strnstr(const char *big, const char *little, size_t len);
+char			*ft_strrchr(const char *s, int c);
+char			**ft_strsplit(char const *s, char c);
+char			*ft_strstr(const char *big, const char *little);
+char			*ft_strsub(char const *s, unsigned int start, size_t len);
+char			*ft_strtrim(char const *s);
+char			*ft_strrev(char *str);
+void			*ft_realloc(void *src, size_t srcsize, size_t newsize);
+char			*ft_strcjoin(char *delim, char **arr, size_t size);
+void			ft_printjoin(char *delim, char **arr, size_t size);
+
+/*
+** Conversions
+*/
+
+int				ft_atoi(const char *nptr);
+char			*ft_itoa(int n);
+double			ft_atof(const char *str);
+
 #endif
