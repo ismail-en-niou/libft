39ecb0f9c60c6f784bb412303648c5bace14e9a8
ismail-en-niou <ismail.enniou@etu.uae.ac.ma>
2024-08-21 14:12:40 +0100

    ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,31 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memmov.c                                        :+:      :+:    :+:   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/21 12:55:46 by marvin            #+#    #+#             */
-/*   Updated: 2024/08/21 12:55:46 by marvin           ###   ########.fr       */
+/*   Created: 2024/08/21 13:03:55 by marvin            #+#    #+#             */
+/*   Updated: 2024/08/21 13:03:55 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dest, const void *src, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
+	unsigned char	*temp;
 
-	i = 0;
-	if (src < dest)
-		ft_memccpy(dest, src, n);
-	else
+	temp = (t_byte *)s;
+	while (n--)
 	{
-		while (i < len)
-		{
-			((char *)dest)[i] = ((char *)src)[i];
-			i++;
-		}
+		if (*(temp++) == (unsigned char )c)
+			return (temp - 1);
 	}
-	return (dest);
+	return (NULL);
 }
