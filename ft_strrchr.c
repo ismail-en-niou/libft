f4b4850dd417ecac35f4e45d3fc05d4cd4c8e2e2
ismail-en-niou <ismail.enniou@etu.uae.ac.ma>
2024-08-22 12:49:30 +0100

    ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,22 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/22 12:31:22 by marvin            #+#    #+#             */
-/*   Updated: 2024/08/22 12:31:22 by marvin           ###   ########.fr       */
+/*   Created: 2024/08/22 12:35:26 by marvin            #+#    #+#             */
+/*   Updated: 2024/08/22 12:35:26 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strchr(const char *s, int c)
+char	*ft_strrchr(const char *s, int c)
 {
-	while (*s != '\0' && *s != (char)c)
-		s++;
-	if (s != (char)c)
-		return (NULL);
-	return ((char *)s);
+	int	i;
+
+	i = ft_strlen(s);
+	if ((char)c == '\0')
+		return ((char *)s + i);
+	while (i > 0)
+	{
+		if (s[i] == (char )c)
+			return ((char *)s + i);
+		i--;
+	}
+	return (NULL);
 }
