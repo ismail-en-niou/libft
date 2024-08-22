0be43c4c374793eb5883adf5961cc9f0534e33b2
ismail-en-niou <ismail.enniou@etu.uae.ac.ma>
2024-08-22 13:04:49 +0100

    ft_strstr.c

---
+++ ft_strstr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/22 12:49:56 by marvin            #+#    #+#             */
+/*   Updated: 2024/08/22 12:49:56 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strstr(const char *haystack, const char *needle)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	while (haystack[i])
+	{
+		j = 0;
+		if (haystack[i] == needle[j])
+		{
+			while (haystack[i + j] == needle[j] && needle[j])
+				j++;
+			if (needle[j] == '\0')
+				return ((char *)(haystack + i));
+		}
+		i++;
+	}
+	return (NULL);
+}
