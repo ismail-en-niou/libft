e11826e85c35a4a9638188432045ca07e7d6a1a2
ismail-en-niou <ismail.enniou@etu.uae.ac.ma>
2024-08-22 11:50:16 +0100

    ft_strncat.c && ft_strcat.c modification

--- ft_strncat.c
+++ ft_strncat.c
@@ -1,26 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/22 11:39:02 by marvin            #+#    #+#             */
-/*   Updated: 2024/08/22 11:39:02 by marvin           ###   ########.fr       */
+/*   Created: 2024/08/22 11:45:10 by marvin            #+#    #+#             */
+/*   Updated: 2024/08/22 11:45:10 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strcat(char *dest, const char *src)
+char	*ft_strncat(char *dest, const char *src, size_t n)
 {
 	char	*rest;
 
 	rest = dest;
 	while (*dest)
 		dest++;
-	while (*src)
+	while (*src && n > 0)
+	{
 		*(dest++) = *(src++);
+		n--;
+	}
 	*dest = 0;
 	return (rest);
 }
