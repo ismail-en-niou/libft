2a383ab4f38fdc28f3be78841533b2780e655cf4
ismail-en-niou <ismail.enniou@etu.uae.ac.ma>
2024-08-22 13:21:20 +0100

    ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,26 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/22 12:49:56 by marvin            #+#    #+#             */
-/*   Updated: 2024/08/22 12:49:56 by marvin           ###   ########.fr       */
+/*   Created: 2024/08/22 13:05:56 by marvin            #+#    #+#             */
+/*   Updated: 2024/08/22 13:05:56 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strstr(const char *haystack, const char *needle)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	int	i;
-	int	j;
+	size_t	i;
+	size_t	j;
 
 	i = 0;
+	j = 0;
 	if (*needle == '\0')
 		return ((char *)haystack);
-	while (haystack[i])
+	while (haystack[i] && n < len)
 	{
 		j = 0;
 		if (haystack[i] == needle[j])
