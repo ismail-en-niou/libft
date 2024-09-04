483986c75340dddf13be6d6db37317fc8db02b8f
ismail-en-niou <ismail.enniou@etu.uae.ac.ma>
2024-09-04 13:50:58 +0100

    ft_strjoin.c

---
+++ ft_strjoin.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 13:43:33 by marvin            #+#    #+#             */
+/*   Updated: 2024/09/04 13:43:33 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*ret;
+
+	if (!s1 || !s2)
+		return (NULL);
+	ret = ft_strnew(ft_strlen(s1) + ft_strlen(s2));
+	if (!ret)
+		return (NULL);
+	ft_strcpy(ret, s1);
+	return (ft_strcpy(ret, s2));
+}
